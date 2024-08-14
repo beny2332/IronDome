14aef662a4d41c8f1a83a8cbf6b2a4480e97b66c
BinyaminYacovson <bj57577@gmail.com>
2024-08-14 17:30:01 +0300

    Reapply "sockets using signalIR"
    This reverts commit 19ab27b86969423dcd6c8cedf9869151822ee064.

---
+++ IronDome/Hubs/RealTime.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace IronDome.Hubs
+{
+    public class RealTime : Hub
+    {
+        public async Task AttackAlert(int id, int rt, string name)
+        {
+            await Clients.All.SendAsync("RedAlert", id, rt, name);
+        }
+    }
+}
